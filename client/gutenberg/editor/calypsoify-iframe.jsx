83622c0d7731cd280471c28de74fbfcca5fbb29d
mmtr <miguel.torres@automattic.com>
2019-03-01 18:06:28 +0100

    Gutenframe: Add support for Press This/Reblog

--- client/gutenberg/editor/calypsoify-iframe.jsx
+++ client/gutenberg/editor/calypsoify-iframe.jsx
@@ -39,6 +39,7 @@ class CalypsoifyIframe extends Component {
 		postId: PropTypes.number,
 		postType: PropTypes.string,
 		duplicatePostId: PropTypes.number,
+		pressThis: PropTypes.object,
 	};
 
 	state = {
@@ -137,6 +138,16 @@ class CalypsoifyIframe extends Component {
 		}
 
 		this.setState( { isMediaModalVisible: false } );
+	};
+
+	pressThis = () => {
+		const { pressThis } = this.props;
+		if ( pressThis ) {
+			this.iframePort.postMessage( {
+				action: 'pressThis',
+				payload: pressThis,
+			} );
+		}
 	};
 
 	render() {
